654a9981047b1b8bb64dd184a5ecea7f1cfc9252
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-10-17 19:22:50 +0300

    Update 5.py

--- laboratornaya 6/5.py
+++ laboratornaya 6/5.py
@@ -1,16 +1,13 @@
+
 import functools
 
 
 def additor(cls):
-    def __getattribute__(self, name):
-        if hasattr(self.__dict__, name):
-            return self.__dict__[name]
-        elif hasattr(cls.__dict__, name) or super().hasattr(cls.__dict__, name):
-            return cls.__dict__[name]
-        else:
-            def __getattr__(self, name):
-                return self.__dict__[name] = None
-
+    def new_getattr(self, name):
+        self.__dict__[name] = None
+        
+    cls.__getattr__ = new_getattr()
+    return cls
 
 
 
