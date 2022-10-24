481a799693933312e16b44384a2a174387acce85
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-10-24 17:32:59 +0300

    Update 1.2.py

--- laboratornaya 8/1.2.py
+++ laboratornaya 8/1.2.py
@@ -1,18 +1,11 @@
 def my_range(n):
     _current = 1
     _current2 =1
-    yield _current
-    i = 1
-    while True:
-        if i < n:
-            yield _current2
-            _next = _current + _current2
-            _current = _current2
-            _current2 = _next
-            i += 1
-        else:
-            break
-
+    for i in range(n):
+        yield _current
+        _next = _current + _current2
+        _current = _current2
+        _current2 = _next
 
 
 for i in my_range(10):
