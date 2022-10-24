208d4c3075ec4e1ff48614276e0c6f6a5fcd524d
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-10-24 22:08:20 +0300

    Create 2.3.py

---
+++ laboratornaya 8/2.3.py
@@ -0,0 +1,13 @@
+import itertools
+
+def get_combinations(s, k):
+    G = []
+    for k in range(k):
+        m = []
+        M = itertools.combinations(s, k+1)
+        for j in M:
+            m.append(''.join(sorted(j)))
+        m.sort()
+        G.extend(m)
+    return G
+print((get_combinations('cat', 2)))
