204f95a51910a4e167d805f26660c0a5424724f0
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-11-04 22:47:35 +0300

    Update 3.py

--- laboratornaya 9/3.py
+++ laboratornaya 9/3.py
@@ -3,19 +3,25 @@ import re
 class TextLoader:
     def __init__(self, ad):
         self.ad = ad
-        self.files = os.listdir(ad)
+        self.files = os.listdir(self.ad)
 
     def __len__(self):
         return len(self.files)
 
-    def __getitem__(self, item):
-        a = open(self.ad + '/' + self.files[item])
-        for i in a:
-            i.lower()
-            re.sub(r'[^\w\s]', '', i)
-        return print(a[8])
+    def __getitem__(self, item): # аналог
+        with open(self.ad + '/' + self.files[item]) as folder:
+            a = folder.read().lower()
+        return re.sub(r'[^\w\s]', '', a)
 
+    def __iter__(self): # итератор, делающий то же, что и getitem
+        for i in self.files:
+            with open(self.ad + '/' + i) as folder:
+                a = folder.read().lower()
+                yield re.sub(r'[^\w\s]', '', a)
 
-out = TextLoader('sample')
-print (len(out))
+out = TextLoader('C:/Users/Didi/Downloads/sample')
+print(len(out))
 print(out[2])
+for i in out:
+    print(i)
+    
