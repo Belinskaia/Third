2a4fec66bb6736da5c884235c144dd57747d8001
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-11-07 20:00:14 +0300

    Update 3.py

--- laboratornaya 10/3.py
+++ laboratornaya 10/3.py
@@ -1,29 +1,12 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from google_images_search import GoogleImagesSearch
+from icrawler.builtin import GoogleImageCrawler
 
-from config import TOKEN
+secret_token = '5635797798:AAHu8ZwtUpR-4GcKbjwadOJwmC3IsQOvPBE'
 
-GCS_DEVELOPER_KEY= 'AIzaSyDbZ0bGtB7NvbGxZRINAXZ5eDuOE4pwcUQ'
-GCS_CX = 'aef786076adc9270f'
-
-bot = Bot(token=TOKEN)
+bot = Bot(token=secret_token)
 dp = Dispatcher(bot)
-gis = GoogleImagesSearch(GCS_DEVELOPER_KEY, GCS_CX)
-
-
-_search_params = {
-    'q': '...',
-    'num': 10,
-    'safe': 'off',
-    'fileType': 'jpg',
-    'imgType': 'photo',
-    'imgSize': 'MEDIUM',
-    'imgDominantColor': 'pink',
-    'imgColorType': 'color',
-    'rights': 'cc_publicdomain'
-}
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
@@ -35,14 +18,11 @@ async def process_help_command(message: types.Message):
 
 @dp.message_handler(commands=['pic'])
 async def photo(message: types.Message):
-	msg = message.text[5:]
-	if len(msg)==0:
-		await bot.send_message(message.from_user.id, "no input")
-	else: await message.answer_photo('https://www.google.com/search?q='+msg+'&tbm=isch')
-
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text) 
+    msg = message.text[5:]
+    if len(msg)==0:
+        await bot.send_message(message.from_user.id, "no input")
+    else:
+        await bot.send_message(message.from_user.id, "https://www.google.com/search?q="+str(msg)+'&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjnxoCVxZz7AhXjlosKHUSzAZAQ_AUoAXoECAIQAw&biw=1920&bih=889&dpr=1')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
