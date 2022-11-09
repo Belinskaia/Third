7e48211a29f1a917fdea36e33287f9be199d062d
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-11-09 22:38:51 +0300

    Update 3.py

--- laboratornaya 10/3.py
+++ laboratornaya 10/3.py
@@ -1,28 +1,23 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from icrawler.builtin import GoogleImageCrawler
 
 secret_token = '5635797798:AAHu8ZwtUpR-4GcKbjwadOJwmC3IsQOvPBE'
-
 bot = Bot(token=secret_token)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
+
+@dp.message_handler(commands=['Salute'])
 async def process_start_command(message: types.Message):
-    await message.reply("Hi! Wanna chat?")
+    await message.reply('Hello, I wait for a request of the form: out main_word')
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("No help here")
-
-@dp.message_handler(commands=['pic'])
+@dp.message_handler(commands=['out'])
 async def photo(message: types.Message):
     msg = message.text[5:]
-    if len(msg)==0:
-        await bot.send_message(message.from_user.id, "no input")
-    else:
-        await bot.send_message(message.from_user.id, "https://www.google.com/search?q="+str(msg)+'&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjnxoCVxZz7AhXjlosKHUSzAZAQ_AUoAXoECAIQAw&biw=1920&bih=889&dpr=1')
+    img = 'https://htstatic.imgsmail.ru/pic_image/0efda7a7a24673153e86521d430076e3/840/526/2269298/'
+    import urllib
+    photo = urllib.urlretrieve(img, "...\img.jpg") 
+    await bot.send_photo(message.from_user.id, photo)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
