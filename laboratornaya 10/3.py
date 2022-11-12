d137502a27f01b1857b5ad18a6507f9a7fffbddf
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-11-13 01:21:59 +0300

    Update 3.py

--- laboratornaya 10/3.py
+++ laboratornaya 10/3.py
@@ -1,23 +1,24 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import requests
+from bs4 import BeautifulSoup
+import lxml
 
-secret_token = '5635797798:AAHu8ZwtUpR-4GcKbjwadOJwmC3IsQOvPBE'
-bot = Bot(token=secret_token)
+bot = Bot(token='5635797798:AAHu8ZwtUpR-4GcKbjwadOJwmC3IsQOvPBE')
 dp = Dispatcher(bot)
 
-
-@dp.message_handler(commands=['Salute'])
+@dp.message_handler(commands=['Hello'])
 async def process_start_command(message: types.Message):
-    await message.reply('Hello, I wait for a request of the form: out main_word')
+    await message.reply("Salute! You can get picture)")
 
-@dp.message_handler(commands=['out'])
+@dp.message_handler()
 async def photo(message: types.Message):
-    msg = message.text[5:]
-    img = 'https://htstatic.imgsmail.ru/pic_image/0efda7a7a24673153e86521d430076e3/840/526/2269298/'
-    import urllib
-    photo = urllib.urlretrieve(img, "...\img.jpg") 
-    await bot.send_photo(message.from_user.id, photo)
+    URL = 'https://images.search.yahoo.com/search/images;_ylt=AwrEqMqhMWljIE40j31XNyoA;_ylu=Y29sbwNiZjEEcG9zAzEEdnRpZANMT0NVSTA1M18xBHNlYwNwaXZz?p='+message.text[3:]+'&fr2=piv-web&fr=yfp-t'
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, 'lxml')
+    q = soup.select('img[src]')
+    await bot.send_photo(message.from_user.id, q[0]['src'])
 
 if __name__ == '__main__':
     executor.start_polling(dp)
