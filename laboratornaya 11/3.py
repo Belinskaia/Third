ae85a2ea765169ff0f556493cbfe371c574f395a
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-11-14 20:06:35 +0300

    Update 3.py

--- laboratornaya 11/3.py
+++ laboratornaya 11/3.py
@@ -1,24 +1,25 @@
 from threading import Thread
 import time
-
+M = [150, 60, 80, 51, 61, 91]
 start = time.time()
-def matter(i):
-    M = [150, 60, 80, 51, 61, 91]
+def matter(i, A):
     global s
     if i == 0:
         s = 0
-    s += M[i]
+    s += sum(A)
 
 def run_threads(count):
     threads = []
+    D = len(M)//count +1
     for i in range(count):
-        t = Thread(target = matter, args=(i,))
+        A = M[i*D:(i+1)*D]
+        t = Thread(target = matter, args=(i, A))
         threads.append(t)
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
 
-run_threads(6)
+run_threads(4)
 print(s)
 print(time.time() - start)
