321888751c29d3ed09e82f8b026b69b4150fb922
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2022-12-02 08:32:53 +0300

    Update 1_client.py

--- laboratornaya 14/1_client.py
+++ laboratornaya 14/1_client.py
@@ -3,9 +3,7 @@
 import sys
 import socket
 import selectors
-from time import sleep
 import types
-import random
 
 sel = selectors.DefaultSelector()
 messages = [b"Message 1 from client.", b"Message 2 from client."]
@@ -22,41 +20,50 @@ def start_connections(host, port, num_conns):
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
         data = types.SimpleNamespace(
             connid=connid,
+            msg_total=sum(len(m) for m in messages),
             recv_total=0,
             messages=messages.copy(),
             outb=b"",
-            name = i+2
         )
         sel.register(sock, events, data=data)
+
 
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data == "Salute! Enter your name":
-            sent = sock.send(data.name)  # Should be ready to write
-            data.outb = data.outb[sent:]
-        if not recv_data:
+        if recv_data:
+            print(f"Received {recv_data!r} from connection {data.connid}")
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
             print(f"Closing connection {data.connid}")
             sel.unregister(sock)
             sock.close()
     if mask & selectors.EVENT_WRITE:
-        m = random.randint(1, 2)
-        if m == 1:
-            sent = sock.send(b'1')
-            data.outb = data.outb[sent:]
-        if m == 2:
-            sent = sock.send(bytes(f'2 {random.randint()}'))  # Should be ready to write
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print(f"Sending {data.outb!r} to connection {data.connid}")
+            sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
 
 
-host, port = '127.0.0.1', 4444
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.sendall(b"1")
-    data = s.recv(1024).decode('ascii')
-    print(f"Received {data}")
-    sleep(1)
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-    print(f"Received {data}")
+client = []
+host, port = '127.0.0.1', 9999
+for i in range(4):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    client.append(s)
+    print(s.recv(1024).decode('ascii'))
+    s.sendall(str(i).encode('ascii'))
+    print(s.recv(1024).decode('ascii'))
+client[0].sendall(b'(1)')
+data = client[0].recv(1024).decode('ascii')
+print(f"Received {data}")
+client[1].sendall(b'(2) Hi, how do you do?._.')
+data = client[2].recv(1024).decode('ascii')
+print(f"Received {data}")
+client[3].sendall(b'(1)')
+data = client[0].recv(1024).decode('ascii')
+print(f"Received {data}")
