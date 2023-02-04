2104ef99ecf25ab85075ee666e5b1d2421bb8047
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2023-02-04 12:31:53 +0300

    Update 2.py

--- laboratornaya 16/2.py
+++ laboratornaya 16/2.py
@@ -3,7 +3,7 @@ import imageio
 import matplotlib.pyplot as plt
 
 img = imageio.imread()
-a = np.array([0.299, 0.587, 0.114])
+a = np.array([0.299, 0.587, 0.114, 1])
 s = img.dot(a)
 plt.imshow(s)
 plt.show()
