4043abd81336f4ac9b63f12987c11d6c6e766c6c
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2023-02-16 00:40:39 +0300

    Create 4.py

---
+++ laboratornaya 18/4.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import warnings
+import matplotlib.pyplot as plt
+warnings.filterwarnings('ignore')
+
+data = pd.read_csv('flight_delays.csv', sep=',')
+for i in range(1, 13, 1):
+    if i in [1, 2, 12]:
+        data.loc[data["Month"] == 'c-'+str(i), "Month"] = 'Winter'
+    elif i in [3, 4, 5]:
+        data.loc[data["Month"] == 'c-' + str(i), "Month"] = 'Spring'
+    elif i in [6, 7, 8]:
+        data.loc[data["Month"] == 'c-'+str(i), "Month"] = 'Summer'
+    else:
+        data.loc[data["Month"] == 'c-'+str(i), "Month"] = 'Autumn'
+data = data.rename(columns={"Month": "Seasons"})
+data_1 = data[data['dep_delayed_15min'] == 'Y'].groupby('Seasons').dep_delayed_15min.count()
+print(data_1)
+data_1.plot(kind='pie', colors = ("orangered", "limegreen", "magenta", "royalblue"))
+plt.show()
