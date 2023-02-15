76b2c7982b05e6bcf5135c2a26ddc773ca74c8e0
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2023-02-15 23:59:12 +0300

    Update 3.py

--- laboratornaya 18/3.py
+++ laboratornaya 18/3.py
@@ -1,9 +1,12 @@
 #топ 5 направлений, для которых чаще всего происходят задержки
 import pandas as pd
 import warnings
+import matplotlib.pyplot as plt
 warnings.filterwarnings('ignore')
 
 data = pd.read_csv('flight_delays.csv', sep=',')
 data_1 = data[data['dep_delayed_15min'] == 'Y'].groupby('Dest').dep_delayed_15min.count()
 D = data_1.nlargest(5)
 print(D)
+D.plot(kind='pie')
+plt.show()
