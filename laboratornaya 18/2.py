419286119f33240c9ed49c97e4a348ae53057c96
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2023-02-15 23:27:56 +0300

    Update 2.py

--- laboratornaya 18/2.py
+++ laboratornaya 18/2.py
@@ -5,6 +5,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 data = pd.read_csv('flight_delays.csv', sep=',')
-data_1 = data[data['dep_delayed_15min'] == 'Y'].groupby('Distance').Origin.count()
+data_1 = data[data['dep_delayed_15min'] == 'Y'].groupby('Distance').Distance.count()
 data_1.plot(kind='line', x='Distance', y='count')
 plt.show()
