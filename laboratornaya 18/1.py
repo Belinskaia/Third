397d9a49d8e752b5a6469b2daa07cbf9d2e8292d
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2023-02-15 23:27:28 +0300

    Update 1.py

--- laboratornaya 18/1.py
+++ laboratornaya 18/1.py
@@ -8,8 +8,8 @@ data = pd.read_csv('flight_delays.csv', sep=',')
 for i in range(1,13,1):
     data.loc[data["Month"] == 'c-'+str(i), "Month"] = i
 data.sort_values(by='Month')
-data_1 = data[data['dep_delayed_15min'] == 'Y'].groupby('Month').Origin.count()
-data_2 = data.groupby('Month').Origin.count()
+data_1 = data[data['dep_delayed_15min'] == 'Y'].groupby('Month').Month.count()
+data_2 = data.groupby('Month').Month.count()
 data_3 = data_1 / data_2
 data_3.plot(kind='bar', x='Month', y='count')
 plt.show()
