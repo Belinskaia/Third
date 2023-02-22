d461a5124b6eeb1ce228a9a5b06182074f2d5b23
Belinskaia <112963615+Belinskaia@users.noreply.github.com>
2023-02-22 21:49:26 +0300

    Update and rename 1-2 to 1-2.py

--- laboratornaya 19/1-2.py
+++ laboratornaya 19/1-2.py
@@ -27,12 +27,14 @@ CREATE TABLE Records(
   	reader_id INT, 
   	taking_date DATE, 
   	returning_date DATE
+	FOREIGN KEY (book_id) REFERENCES Books (id),
+  	FOREIGN KEY (reader_id) REFERENCES Readers (id)
 );
 INSERT INTO Records
 VALUES
     (1, 2, '2020-02-04', '2020-06-08'),
     (2, 3, '2020-10-10', '2020-11-08'),
-    (3, 1, '2021-01-18', '2021-04-14'),
+    (3, 1, '2021-01-18', NULL),
     (4, 3, '2021-08-12', '2021-10-16'),
     (5, 1, '2022-03-13', '2022-02-28');
     
@@ -47,7 +49,7 @@ SELECT id, title
 FROM Books
 INNER JOIN Records
     ON Books.id = Records.book_id
-    WHERE DATETIME("now") BETWEEN Records.taking_date AND Records.returning_date;
+    WHERE Records.taking_date IS NOT NULL AND Records.returning_date IS NULL;
 
 #2.2
 SELECT name, title
